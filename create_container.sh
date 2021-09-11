7cad00dda32b70a1a0c3c2bd1e7257ebf0277a77
jayroo1976 <80148368+jayroo1976@users.noreply.github.com>
2021-09-11 11:31:38 +0400

    Update create_container.sh

--- create_container.sh
+++ create_container.sh
@@ -211,7 +211,7 @@ pct unmount $CTID && unset MOUNT
 # Setup container for tuya-convert
 msg "Starting LXC container..."
 pct start $CTID
-# pct push $CTID commit_switcher.sh /root/commit_switcher.sh -perms 755
+pct push $CTID commit_switcher.sh /root/commit_switcher.sh -perms 755
 pct push $CTID configure_tuya-convert.sh /root/configure_tuya-convert.sh -perms 755
 pct push $CTID install_tuya-convert.sh /root/install_tuya-convert.sh -perms 755
 pct push $CTID login.sh /root/login.sh -perms 755
